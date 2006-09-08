8ef5f1c156a047f56336ce2b90fe3407c3747907
morgan3d <morgan3d>
2006-09-08 17:27:24 +0000

    *** empty log message ***

--- gfxmeter/source/main.cpp
+++ gfxmeter/source/main.cpp
@@ -4,12 +4,10 @@
   @author Morgan McGuire, matrix@graphics3d.com
  */
 
-#define NO_SDL_MAIN
 #include "G3D/G3DAll.h"
 #include "GLG3D/GLG3D.h"
-
-#if G3D_VER < 70000
-//#    error Requires G3D 7.00
+#ifdef main
+#    undef main
 #endif
 
 #include "Report.h"
--- test/main.cpp
+++ test/main.cpp
@@ -490,7 +490,7 @@ int main(int argc, char* argv[]) {
 #    ifndef _DEBUG
         printf("Performance analysis:\n\n");
 
-		perfCollisionDetection();
+	perfCollisionDetection();
 
         perfArray();
 
@@ -513,8 +513,7 @@ int main(int argc, char* argv[]) {
         measureMemsetPerformance();
         measureNormalizationPerformance();
 
-
-		GWindow::Settings settings;
+	GWindow::Settings settings;
         settings.width = 800;
         settings.height = 600;
         settings.alphaBits = 0;
@@ -535,7 +534,7 @@ int main(int argc, char* argv[]) {
 
         measureRDPushPopPerformance(renderDevice);
 
-		getch();
+	getch();
 
 #   else
 
