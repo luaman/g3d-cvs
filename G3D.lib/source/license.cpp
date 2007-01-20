a66facdeccd6d0380832934b5247c44df3a10776
morgan3d <morgan3d>
2007-01-20 22:57:23 +0000

    Removed SDL/GPL from license on Win32

--- G3D.lib/source/license.cpp
+++ G3D.lib/source/license.cpp
@@ -15,17 +15,14 @@ namespace G3D {
 std::string license() {
     return format(
 
-"This program uses the Graphics3D Library (\"G3D\", http://g3d-cpp.sf.net)\n"
 "This software is based in part on the PNG Reference Library which is\n"
-"Copyright (c) 2004 Glenn Randers-Pehrson\n"
-"This software is based in part on the work of the Independent JPEG Group.\n"
-"This software uses the Simple DirectMedia Layer library (\"SDL\",\n"
-"http://www.libsdl.org), which is included under the terms of the\n"
-"GNU Lesser General Public License, (http://www.gnu.org/copyleft/lesser.html).\n"
-"\n"
-"This program uses the Graphics3D Library (\"G3D\", http://g3d-cpp.sf.net), which\n"
+"Copyright (c) 2004 Glenn Randers-Pehrson\n\n"
+"This software is based in part on the work of the Independent JPEG Group.\n\n"
+"%s"
+"This program uses the G3D Library (http://g3d-cpp.sf.net), which\n"
 "is licensed under the \"BSD\" Open Source license.  The Graphics3D library\n"
 "source code is Copyright © 2000-2006, Morgan McGuire, All rights reserved.\n"
+"The BSD license requires the following statement regarding G3D:\n"
 "\n"
 "Redistribution and use in source and binary forms, with or without\n"
 "modification, are permitted provided that the following conditions\n"
@@ -38,8 +35,8 @@ std::string license() {
 "notice, this list of conditions and the following disclaimer in the\n"
 "documentation and/or other materials provided with the distribution.\n" 
 "\n"
-"Neither the name of Morgan McGuire, Brown University, nor the names\n"
-"of its contributors may be used to endorse or promote products derived\n"
+"Neither the name of Morgan McGuire, Brown University, Williams College, nor the names\n"
+"of the G3D contributors may be used to endorse or promote products derived\n"
 "from this software without specific prior written permission.\n"
 "\n"
 "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n"
@@ -54,7 +51,17 @@ std::string license() {
 "(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n"
 "OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
 "\n\n"
-"G3D VERSION %d\n", G3D_VER);
+"G3D VERSION %d\n", 
+
+#ifdef G3D_WIN32
+    "" // Win32 doesn't use SDL
+#else
+    "This software uses the Simple DirectMedia Layer library (\"SDL\",\n"
+    "http://www.libsdl.org), which is included under the terms of the\n"
+    "GNU Lesser General Public License, (http://www.gnu.org/copyleft/lesser.html).\n\n"
+#endif
+,
+G3D_VER);
 }
 
 }
