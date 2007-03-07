c83d593418154b73f07177b71c55e533d680c2ec
morgan3d <morgan3d>
2007-03-07 21:34:47 +0000

    *** empty log message ***

--- test/tfilter.cpp
+++ test/tfilter.cpp
@@ -8,7 +8,7 @@ void testfilter() {
     debugAssert(fuzzyEq(coeff[0], 0.00026386508));
     debugAssert(fuzzyEq(coeff[1], 0.10645077));
     debugAssert(fuzzyEq(coeff[2], 0.78657067));
-    debugAssert(fuzzyEq(coeff[3], 0.106645077));
+    debugAssert(fuzzyEq(coeff[3], 0.10645077));
     debugAssert(fuzzyEq(coeff[4], 0.00026386508));
 
     printf("passed\n");
