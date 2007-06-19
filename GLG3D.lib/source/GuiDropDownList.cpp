a238abc3220189ec347e287843e9824560446cbf
morgan3d <morgan3d>
2007-06-19 06:33:23 +0000

    DropDownList allows keyboard scrolling

--- GLG3D.lib/source/GuiDropDownList.cpp
+++ GLG3D.lib/source/GuiDropDownList.cpp
@@ -4,7 +4,7 @@
  @maintainer Morgan McGuire, morgan@graphics3d.com
 
  @created 2007-06-02
- @edited  2007-06-10
+ @edited  2007-06-19
  */
 #include "G3D/platform.h"
 #include "GLG3D/GuiDropDownList.h"
@@ -24,6 +24,7 @@ GuiDropDownList::GuiDropDownList
                                           m_selecting(false) {
 }
 
+
 void GuiDropDownList::render(RenderDevice* rd, const GuiSkinRef& skin) const {
     if (m_visible) {
         skin->renderDropDownList(m_rect, m_enabled, focused() || mouseOver(), m_selecting, m_listValue[*m_indexValue], m_caption);
@@ -32,14 +33,29 @@ void GuiDropDownList::render(RenderDevice* rd, const GuiSkinRef& skin) const {
 
 
 bool GuiDropDownList::onEvent(const GEvent& event) {
-    if ((event.type == GEventType::MOUSE_BUTTON_DOWN) && m_visible) {
+    if (! m_visible) {
+        return false;
+    }
+
+    if (event.type == GEventType::MOUSE_BUTTON_DOWN) {
         // TODO: handle selection TODO: GuiWindow must support an
         // active "menu" that can float over everything and steal all
         // events
         return true;
-    } else {
-        return false;
+    } else if (event.type == GEventType::KEY_DOWN) {
+        switch (event.key.keysym.sym) {
+        case GKey::DOWN:
+            *m_indexValue = iMin(*m_indexValue + 1, m_listValue.size() - 1);
+            return true;
+
+        case GKey::UP:
+            *m_indexValue = iMax(*m_indexValue - 1, 0);
+            return true;
+        default:;
+        }
     }
+
+   return false;
 }
 
 
