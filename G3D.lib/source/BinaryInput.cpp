849fabf8381de02a63470e790e24e7a982fb6c8d
corey_taylor <corey_taylor>
2007-12-06 04:30:00 +0000

    compile fix

--- G3D.lib/source/BinaryInput.cpp
+++ G3D.lib/source/BinaryInput.cpp
@@ -159,12 +159,12 @@ void BinaryInput::loadIntoMemory(int64 startPosition, int64 minLength) {
         file = NULL;
     
 #   else
-        FILE* file = fopen(filename.c_str(), "rb");
+        FILE* file = fopen(m_filename.c_str(), "rb");
         debugAssert(file);
         int ret = fseeko(file, (off_t)m_alreadyRead, SEEK_SET);
         debugAssert(ret == 0);
         size_t toRead = (size_t)G3D::min<int64>(m_bufferLength, m_length - m_alreadyRead);
-        ret = fread(buffer, 1, toRead, file);
+        ret = fread(m_buffer, 1, toRead, file);
         debugAssert((size_t)ret == (size_t)toRead);
         fclose(file);
         file = NULL;
