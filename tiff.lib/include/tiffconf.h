af3f03251918dba6e83662035f2066291b266b31
morgan3d <morgan3d>
2009-11-10 16:14:24 +0000

    *** empty log message ***

---
+++ tiff.lib/include/tiffconf.h
@@ -0,0 +1,6 @@
+#ifdef _MSC_VER
+#   include "tiffconf.vc.h"
+#else
+#pragma message("Need a tiffconf.h for this platform")
+#   include "tiffconf.vc.h"
+#endif
---
+++ tiff.lib/source/tif_config.h
@@ -0,0 +1,6 @@
+#ifdef _MSC_VER
+#   include "tif_config.vc.h"
+#else
+#pragma message("Need a tif_config.h for this platform")
+#   include "tif_config.vc.h"
+#endif
