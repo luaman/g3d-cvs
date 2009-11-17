3f779be4e50c23479549d2a60d38d183b11c18d4
morgan3d <morgan3d>
2009-11-17 02:20:45 +0000

    *** empty log message ***

--- G3D.lib/include/G3D/HashTrait.h
+++ G3D.lib/include/G3D/HashTrait.h
@@ -3,13 +3,14 @@
 
   @maintainer Morgan McGuire, morgan@cs.williams.edu
   @created 2008-10-01
-  @edited  2008-10-01
+  @edited  2009-11-01
+
   Copyright 2000-2009, Morgan McGuire.
   All rights reserved.
  */
 
-#ifndef G3D_HASHTRAIT_H
-#define G3D_HASHTRAIT_H
+#ifndef G3D_HashTrait_h
+#define G3D_HashTrait_h
 
 #include "G3D/platform.h"
 #include "G3D/Crypto.h"
@@ -42,22 +43,22 @@ template <> struct HashTrait <std::string> {
 };
 
 template <> struct HashTrait<G3D::uint128> {
-	// Use the FNV-1 hash (http://isthe.com/chongo/tech/comp/fnv/#FNV-1).
+    // Use the FNV-1 hash (http://isthe.com/chongo/tech/comp/fnv/#FNV-1).
     static size_t hashCode(G3D::uint128 key) {
         static const G3D::uint128 FNV_PRIME_128(1 << 24, 0x159);
         static const G3D::uint128 FNV_OFFSET_128(0xCF470AAC6CB293D2ULL, 0xF52F88BF32307F8FULL);
 
         G3D::uint128 hash = FNV_OFFSET_128;
         G3D::uint128 mask(0, 0xFF);
-		for (int i = 0; i < 16; ++i) {
-			hash *= FNV_PRIME_128;
+        for (int i = 0; i < 16; ++i) {
+            hash *= FNV_PRIME_128;
             hash ^= (mask & key);
             key >>= 8;
-		}
-		
+        }
+	
         G3D::uint64 foldedHash = hash.hi ^ hash.lo;
-		return static_cast<size_t>((foldedHash >> 32) ^ (foldedHash & 0xFFFFFFFF));
-	}
+        return static_cast<size_t>((foldedHash >> 32) ^ (foldedHash & 0xFFFFFFFF));
+    }
 };
 
 #endif
