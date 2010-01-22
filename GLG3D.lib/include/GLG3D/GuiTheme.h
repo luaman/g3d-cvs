68a63e96766943ada455b5785e1edd98dbe278c8
corey_taylor <corey_taylor>
2010-01-22 05:17:20 +0000

    Fixed example code so it compiles.

--- GLG3D.lib/include/GLG3D/GuiTheme.h
+++ GLG3D.lib/include/GLG3D/GuiTheme.h
@@ -61,25 +61,25 @@ typedef ReferenceCountedPointer<class GuiTheme> GuiThemeRef;
 
   <pre>
    theme->beginRendering(rd);
-       theme->renderWindow(rd, Rect2D::xywh(80, 70, 550, 250), false, false, false, "Window", GuiTheme::NORMAL_WINDOW_STYLE);
-       theme->renderWindow(rd, Rect2D::xywh(500, 120, 50, 150), true, true, true, "Tools", GuiTheme::TOOL_WINDOW_STYLE);
-       theme->renderCheckBox(rd, Rect2D::xywh(100, 100, 20, 20), true, true, true, "Check box");
-       theme->renderRadioButton(rd, Rect2D::xywh(100, 120, 20, 20), true, false, false, "Radio button");
-       theme->renderButton(rd, Rect2D::xywh(100, 160, 80, 27), true, true, true, "Button");
+       theme->renderWindow(Rect2D::xywh(80, 70, 550, 250), false, false, false, false, "Window", GuiTheme::NORMAL_WINDOW_STYLE);
+       theme->renderWindow(Rect2D::xywh(500, 120, 50, 150), true, true, true, "Tools", GuiTheme::TOOL_WINDOW_STYLE);
+       theme->renderCheckBox(Rect2D::xywh(100, 100, 20, 20), true, true, true, "Check box");
+       theme->renderRadioButton(Rect2D::xywh(100, 120, 20, 20), true, false, false, "Radio button");
+       theme->renderButton(Rect2D::xywh(100, 160, 80, 27), true, true, true, "Button");
        theme->setFont(timesFont, 12, Color3::black(), Color4::clear());
-       theme->renderButton(rd, Rect2D::xywh(200, 160, 80, 27), true, true, true, "New Font");
+       theme->renderButton(Rect2D::xywh(200, 160, 80, 27), true, true, true, "New Font");
        theme->setFont(arialFont, 10, Color3::black(), Color4::clear());
-       theme->renderHorizontalSlider(rd, Rect2D::xywh(100, 200, 150, 27), 0.5f, true, false, "Slider");
+       theme->renderHorizontalSlider(Rect2D::xywh(100, 200, 150, 27), 0.5f, true, false, "Slider");
 
        int s = 30;
-       theme->renderButton(rd, Rect2D::xywh(100 + s * 0, 230, 30, 30), true, false, false, GuiText("7", iconFont));
-       theme->renderButton(rd, Rect2D::xywh(100 + s * 1, 230, 30, 30), true, false, false, GuiText("4", iconFont));
-       theme->renderButton(rd, Rect2D::xywh(100 + s * 2, 230, 30, 30), true, false, false, GuiText("=", iconFont));
-       theme->renderButton(rd, Rect2D::xywh(100 + s * 3, 230, 30, 30), true, false, true, GuiText(";", iconFont));
-       theme->renderButton(rd, Rect2D::xywh(100 + s * 4, 230, 30, 30), true, false, false, GuiText("<", iconFont));
-       theme->renderButton(rd, Rect2D::xywh(100 + s * 5, 230, 30, 30), true, false, false, GuiText("8", iconFont));
+       theme->renderButton(Rect2D::xywh(100 + s * 0, 230, 30, 30), true, false, false, GuiText("7", iconFont));
+       theme->renderButton(Rect2D::xywh(100 + s * 1, 230, 30, 30), true, false, false, GuiText("4", iconFont));
+       theme->renderButton(Rect2D::xywh(100 + s * 2, 230, 30, 30), true, false, false, GuiText("=", iconFont));
+       theme->renderButton(Rect2D::xywh(100 + s * 3, 230, 30, 30), true, false, true, GuiText(";", iconFont));
+       theme->renderButton(Rect2D::xywh(100 + s * 4, 230, 30, 30), true, false, false, GuiText("<", iconFont));
+       theme->renderButton(Rect2D::xywh(100 + s * 5, 230, 30, 30), true, false, false, GuiText("8", iconFont));
        theme->setFont(arialFont, 10, Color3::black(), Color4::clear());
-    theme->endRendering(rd);
+    theme->endRendering();
    </pre>
   @sa G3D::GuiWindow
 */
