287cfc412d26ca2e5023b71f177bbfc225dd5e86
morgan3d <morgan3d>
2010-02-05 18:47:34 +0000

    *** empty log message ***

--- demos/starter/App.cpp
+++ demos/starter/App.cpp
@@ -123,10 +123,14 @@ void App::onGraphics3D(RenderDevice* rd, Array<Surface::Ref>& surface3D) {
     // ShadowMap as the final argument to create shadows.)
     Surface::sortAndRender(rd, defaultCamera, surface3D, m_scene->lighting(), m_shadowMap);
 
+    //////////////////////////////////////////////////////
     // Sample immediate-mode rendering code
     rd->enableLighting();
     for (int i = 0; i < m_scene->lighting()->lightArray.size(); ++i) {
         rd->setLight(i, m_scene->lighting()->lightArray[i]);
+    }
+    for (int i = 0; i < m_scene->lighting()->shadowedLightArray.size(); ++i) {
+        rd->setLight(i + m_scene->lighting()->lightArray.size(), m_scene->lighting()->shadowedLightArray[i]);
     }
     rd->setAmbientLightColor(m_scene->lighting()->ambientAverage());
 
