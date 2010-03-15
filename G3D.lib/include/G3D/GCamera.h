a152b17bb1cdef4202722e2fb002e7f7b92c114b
morgan3d <morgan3d>
2010-03-15 16:56:54 +0000

    *** empty log message ***

--- G3D.lib/include/G3D/GCamera.h
+++ G3D.lib/include/G3D/GCamera.h
@@ -38,6 +38,9 @@ class Any;
 
   All viewport arguments are the pixel bounds of the viewport-- e.g.,
   RenderDevice::viewport().
+
+  See http://bittermanandy.wordpress.com/2009/04/10/a-view-to-a-thrill-part-one-camera-concepts/
+  for a nice introduction to camera transformations.
  */
 class GCamera  {
 
