3921b6059e560449dfb1cf5b5497886de05524e3
morgan3d <morgan3d>
2010-03-19 10:05:48 +0000

    *** empty log message ***

--- GLG3D.lib/include/GLG3D/SuperBSDF.h
+++ GLG3D.lib/include/GLG3D/SuperBSDF.h
@@ -22,7 +22,7 @@ namespace G3D {
    approximation for glossy and mirror reflection, Sloan, Hoffman, and Lafortune's
    normalization of the Blinn-Phong specular lobe, and transmission
    (without exponential extinction) terms.  It is a extension of the isotropic
-   version of Ashikman and Shirley's empirical BRDF http://www.cs.utah.edu/~shirley/papers/jgtbrdf.pdf.
+   version of Ashikhmin and Shirley's empirical BRDF http://www.cs.utah.edu/~shirley/papers/jgtbrdf.pdf .
 
    The methods of this class are primarily used for photon mapping,
    ray tracing, and software rasterization.  The G3D::Material class
