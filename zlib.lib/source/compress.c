50404a6d69a04a02a89e3c41826c53854209db61
corey_taylor <corey_taylor>
2010-05-23 18:16:30 +0000

    Upgraded zlib to 1.2.5

--- zlib.lib/source/compress.c
+++ zlib.lib/source/compress.c
@@ -1,9 +1,9 @@
 /* compress.c -- compress a memory buffer
- * Copyright (C) 1995-2003 Jean-loup Gailly.
+ * Copyright (C) 1995-2005 Jean-loup Gailly.
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-/* @(#) $Id: compress.c,v 1.1 2006/08/27 01:58:02 morgan3d Exp $ */
+/* @(#) $Id: compress.c,v 1.2 2010/05/23 18:16:31 corey_taylor Exp $ */
 
 #define ZLIB_INTERNAL
 #include "zlib.h"
@@ -75,5 +75,6 @@ int ZEXPORT compress (dest, destLen, source, sourceLen)
 uLong ZEXPORT compressBound (sourceLen)
     uLong sourceLen;
 {
-    return sourceLen + (sourceLen >> 12) + (sourceLen >> 14) + 11;
+    return sourceLen + (sourceLen >> 12) + (sourceLen >> 14) +
+           (sourceLen >> 25) + 13;
 }
